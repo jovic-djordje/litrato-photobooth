9bab1d0a563ae95e1ce823593b99d7266b855adc
jovic-djordje <jovicdjordje21@gmail.com>
2026-07-07 20:25:19 +0200

    working on homepage hero section

--- src/assets/images.jsx
+++ src/assets/images.jsx
@@ -1,7 +1,17 @@
 import logo from "./logo.png";
+import heroTwo from "./heroTwo.webp";
+import heroOne from "./heroOne.webp";
 
 const Logo = ({ className }) => {
   return <img src={logo} alt="logo" className={className} />;
 };
 
-export { Logo };
+const HeroTwo = ({ className }) => {
+  return <img src={heroTwo} alt="" className={className} />;
+};
+
+const HeroOne = ({ className }) => {
+  return <img src={heroOne} alt="" className={className} />;
+};
+
+export { Logo, HeroTwo, HeroOne };
